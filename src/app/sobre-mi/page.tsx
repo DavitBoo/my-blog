fb96e01ae480d6c777b75daa73d393394f878edd
DavitBoo <davidmarras@gmail.com>
2025-03-20 22:27:40 +0100

    about --on construccion

--- src/app/sobre-mi/page.tsx
+++ src/app/sobre-mi/page.tsx
@@ -1,7 +1,18 @@
+import styles from './sobre-mi.module.css'
 import React from 'react'
 
-export const page = () => {
-  return (
-    <div>page</div>
-  )
+const Page = () => {
+    return (
+        <div className={styles.container}>
+            <main className={styles.main}>  
+                <h1 className={styles.title}>Próximamente</h1>
+                <p className={styles.description}>
+                    Esta sección está en construcción. ¡Vuelve pronto para conocer más sobre mí!
+                </p>
+                <div className={styles.icon}>🚧</div>
+            </main>
+        </div>
+    )
 }
+
+export default Page
