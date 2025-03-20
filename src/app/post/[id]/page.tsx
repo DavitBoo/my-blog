ccdc38c6695753039576e8a055338d0ae546c4c3
DavitBoo <davidmarras@gmail.com>
2025-03-20 22:28:40 +0100

    full modified single post page

--- src/app/post/[id]/page.tsx
+++ src/app/post/[id]/page.tsx
@@ -1,15 +1,16 @@
-import { fetchPostById } from "../../utils/api";
+import { fetchPostById, fetchLabels } from "../../utils/api";
 import CommentSection from "../../components/CommentSection";
+import BackButton from "../../components/BackButton";
 
 import { format } from "date-fns";
-import { decode } from 'html-entities';
+import { decode } from "html-entities";
 import { es } from "date-fns/locale";
-
-import BackButton from "../../components/BackButton";
 
 import { ILabel } from "../../../interfaces/Label";
 
-import { FaGlasses } from 'react-icons/fa';
+import { FaGlasses } from "react-icons/fa";
+
+import Image from "next/image";
 
 type PostProps = {
   params: { id: string };
@@ -20,9 +21,11 @@ const PostPage = async ({ params }: PostProps) => {
   const post = await fetchPostById(postId);
 
   if (!post) {
-    return <div className="no-article" aria-live="assertive">
-    No se ha encontrado este artículo
-  </div>;
+    return (
+      <div className="no-article" aria-live="assertive">
+        No se ha encontrado este artículo
+      </div>
+    );
   }
 
   const decodedString = decode(post?.content);
@@ -34,17 +37,11 @@ const PostPage = async ({ params }: PostProps) => {
     return time;
   };
 
-  console.log(post);
+  const labels = await fetchLabels();
 
   return (
     <main className="article-page">
       <div className="container">
-        <header className="article-header" aria-label="Article Header">
-          <div className="timestamp">
-            <time>{format(new Date(post.createdAt), "EEEE, dd MMMM yyyy", { locale: es })}</time>
-          </div>
-          <span className="author">by {post?.author?.name}</span>
-        </header>
         <ul className="label-list">
           {post?.labels?.map((label: ILabel) => (
             <li key={label.id.toString()} className="label-list-item">
@@ -52,15 +49,34 @@ const PostPage = async ({ params }: PostProps) => {
             </li>
           ))}
         </ul>
-        <h1 className="article-title">{post.title}</h1>
-        <div className="reading-time">
-          <FaGlasses /> {readingTime()} min
+        <h1 className="article-title text-center">{post.title}</h1>
+        <div className="d-flex align-items-center justify-content-center gap-6">
+          <div className="reading-time">
+            <FaGlasses /> {readingTime()} min
+          </div>
+          <header className="article-header" aria-label="Article Header">
+            <div className="timestamp">
+              <time>{format(new Date(post.createdAt), "EEEE, dd MMMM yyyy", { locale: es })}</time>
+            </div>
+            {/* <span className="author">by {post?.author?.name}</span> */}
+          </header>
         </div>
-        
+        <Image src="/placeholder.jpg" alt="Placeholder" width={200} height={150} className="featured-image" />
+
         <div className="article-content" dangerouslySetInnerHTML={{ __html: post.content }} />
         <BackButton />
         <small>Published on {new Date(post.createdAt).toLocaleDateString()}</small>
         <CommentSection postId={post.id} />
+        <div className="container">
+          <h3>Otros temas que trato en el blog:</h3>
+          <ul className="label-list">
+            {labels?.map((label: ILabel) => (
+              <li key={label.id.toString()} className="label-list-item">
+          {label.name}
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </main>
   );
